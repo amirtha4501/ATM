f8d6682f945dda1b3446a01706516eee94fd2314
Amirtha <amirtha4501@gmail.com>
2019-12-02 23:04:49 +0530

    Modified commit

--- transaction3.py
+++ transaction3.py
@@ -1,7 +1,8 @@
 import abc
 from abc import ABC, abstractmethod
+import json
 
-
+# Abstract Class
 class AbstractTransaction(ABC):
     # Abstract Methods
 
@@ -21,32 +22,13 @@ class AbstractTransaction(ABC):
     def debit(self):
         pass
 
-    # @abc.abstractproperty
-    # def hello(self):
-    #     pass
 
 class Account(AbstractTransaction):
     def __init__(self):
-        self.accounts = {
-        "12345": {
-            'name': 'ammu',
-            'balance': 20000,
-            'upi': 1234,
-            'phone': 9095999111
-        },
-        "23456": {
-            'name': 'harshu',
-            'balance': 30000,
-            'upi': 2345,
-            'phone': 9629199923
-        },
-        "34567": {
-            'name': 'madhu',
-            'balance': 40000,
-            'upi': 3456,
-            'phone': 9944468585
-        }
-    }
+        # Reads the json file 
+        with open('details.json','r') as f:
+            self.accounts = json.load(f)
+        # Variable that can be accessed throughout the program
         self.sender_name = 0
         self.receiver_name = 0
         self.sender_acc = 0
@@ -60,8 +42,8 @@ class Account(AbstractTransaction):
         self.phone = 0
         self.balance = 0        
 
-    def __check(self):
-        # if self.sender_acc in self.accounts.keys(): # Checks the accno of sender's presence in dictionary called accounts
+    # Private method that implements encapsulation to check the account's existance.
+    def __check(self):            
         if self.sender_acc in self.accounts.keys(): # Checks the accno of sender's presence in dictionary called accounts
             if self.upi_accesser in self.accounts[self.sender_acc].values():# Compare the upi of sender with the dictionary which is nested
                 return True
@@ -70,49 +52,57 @@ class Account(AbstractTransaction):
         else:
             return False
 
+    # Login method
     def login(self):
-        try:                
-            # print("\n==================================================\n\t\tWELCOME TO BANKING\n==================================================")
-            self.sender_acc = input("\nEnter account number: ")
-            self.upi_accesser = int(input("Enter Password: "))
+        self.sender_acc = input("\nEnter account number: ")
+        self.upi_accesser = int(input("Enter Password: "))
            
-            a = self.__check()
-            if a == True:
-                self.manage_account()
-            else:
-                print("Account number or password is incorrect.")
-                self.login()
-        except:
-            print("\nEnter valid Account number.")
+        a = self.__check()
+        if a == True:
+            self.manage_account()
+        else:
+            print("Account number or password is incorrect.")
             self.login()
 
+    # Creates an account
     def register(self):
+        # Getting input
         self.sender_acc = input("\nEnter account number: ")
         self.upi_accesser = int(input("Enter password: "))
         self.sender_name = input("Enter your name: ")
         self.sender_balance = int(input("Enter your starting balance: "))
         self.phone = int(input("Enter mobile number: "))
-        data = {
+        # Opens file in read mode
+        with open('details.json','r') as f:
+            self.accounts = json.load(f)
+        a = {
             self.sender_acc : {
                 'name' : self.sender_name,
                 'balance' : self.sender_balance,
                 'upi' : self.upi_accesser,
                 'phone' : self.phone
             }
-        }
-        self.accounts.update(data)
+        } 
+        self.accounts.update(a) # Updates the dictionary
+        with open('details.json', 'w') as f:
+            json.dump(self.accounts, f, indent=4)
 
+    # Transfers money from one user to another
     def amount_transfer(self):       
-        self.sender_balance = self.accounts[self.sender_acc]['balance']# accesses sender's balance
+        self.sender_balance = self.accounts[self.sender_acc]['balance'] # accesses sender's balance
         self.money_value = int(input("\nEnter amount to transfer: "))
-        if self.money_value < self.sender_balance:
+        if self.money_value < self.sender_balance: 
             self.receiver_acc = input("\nEnter receiver's account number: ")
-            if self.receiver_acc in self.accounts.keys():# Receiver's account presence in accounts dict 
+            if self.receiver_acc in self.accounts.keys(): # Receiver's account presence in accounts dict 
                 self.sender_balance = self.sender_balance - self.money_value
-                self.receiver_balance = self.accounts[self.receiver_acc]['balance']#accesses receiver's balance
+                self.receiver_balance = self.accounts[self.receiver_acc]['balance'] #accesses receiver's balance
                 self.receiver_balance = self.receiver_balance + self.money_value
                 self.sender_name = self.accounts[self.sender_acc]['name'].title()
-                self.receiver_name = self.accounts[self.receiver_acc]['name'].title()
+                self.receiver_name = self.accounts[self.receiver_acc]['name'].title() 
+                self.accounts[self.sender_acc]['balance'] = self.sender_balance
+                self.accounts[self.receiver_acc]['balance'] = self.receiver_balance
+                with open('details.json','w') as f:
+                    json.dump(self.accounts, f, indent=4)
                 print("\n\n__________________________________________________")
                 print("\n**************Transaction successful**************")
                 print("__________________________________________________\n")
@@ -128,31 +118,33 @@ class Account(AbstractTransaction):
             print("Dear customer,\n\tTransaction is failed since you have insufficient balance.")
         print("\n__________________________________________________\n")
 
+    # To find mini statement
     def miniStatement(self):
         print("\n\n__________________________________________________")
         print("\t\tMINI STATEMENT")
         print("__________________________________________________\n")
         print("Hi",self.accounts[self.sender_acc]['name'].title()+"!\n",)
         print("Amount transfer count : ", self.count)
-        # for(i=0;)
         print("Debit :", self.deb_count)
         print("Credit :", self.cre_count)
         self.tot_trans = self.count + self.deb_count + self.cre_count
         print("Total Transaction : ", self.tot_trans)
         print("\n__________________________________________________\n")
 
+    # Withdrawing the amount
     def debit(self):
         debit_val = int(input("\nEnter amount to withdraw: "))
         self.sender_balance = self.accounts[self.sender_acc]['balance']
-        # print(self.sender_balance)
         if(debit_val <= self.sender_balance):
             print("\n__________________________________________________")
             print("\n\t\tDEBIT SUCCESSFUL")        
             print("__________________________________________________\n")
             self.sender_balance = self.sender_balance - debit_val
-            # print("Dear customer, your current balance is: ",self.sender_balance)
             print("\nDear", self.accounts[self.sender_acc]['name'].title() + "!\n\n\tYour current balance is: ",self.sender_balance)            
             self.deb_count = self.deb_count + 1
+            self.accounts[self.sender_acc]['balance'] = self.sender_balance
+            with open('details.json','w') as f:
+                json.dump(self.accounts, f, indent=4)
         else:
             print("\n__________________________________________________")
             print("\n\t\tDEBIT FAILED")        
@@ -160,7 +152,9 @@ class Account(AbstractTransaction):
             print("\nDear customer, Transaction is failed since you have insufficient balance.")
         print("\n__________________________________________________\n")
 
+    # Deposit the amount
     def credit(self):
+        self.sender_balance = self.accounts[self.sender_acc]['balance']
         credit_val = int(input("\n\nEnter amount to deposit: "))
         if credit_val > 1:
             self.sender_balance = self.sender_balance + credit_val
@@ -169,6 +163,9 @@ class Account(AbstractTransaction):
             print("__________________________________________________\n")
             print("\nDear", self.accounts[self.sender_acc]['name'].title() + "!\n\n\tYour current balance is: ",self.sender_balance)
             self.cre_count = self.cre_count + 1
+            self.accounts[self.sender_acc]['balance'] = self.sender_balance
+            with open('details.json','w') as f:
+                json.dump(self.accounts, f, indent=4)
         else:
             print("\n__________________________________________________")
             print("\n\t\tDECLINED")
@@ -176,11 +173,8 @@ class Account(AbstractTransaction):
             print("Credit amount is not sufficient.")
         print("\n__________________________________________________\n")
 
-    # def hello(self):
-        # print('hello')
-
+    # Method that call the methods for operation
     def manage_account(self):
-        # self.hello()
         while True:
             try:
                 print("\n\t\t1. Amount transfer\n\t\t2. Debit\n\t\t3. Credit\n\t\t4. Ministatement\n\t\t5. Exit")
@@ -200,6 +194,7 @@ class Account(AbstractTransaction):
             except:
                 print("\nEnter valid choice")       
 
+    # Method for login or register option
     def choose(self):
         print("\n==================================================\n\t\tWELCOME TO BANKING\n==================================================")
         while True:
@@ -218,5 +213,3 @@ class Account(AbstractTransaction):
                 print("\nInvalid entry")
 ac = Account()
 ac.choose()
-# ac.register()
-# ac.login()
